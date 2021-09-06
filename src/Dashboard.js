b628bd5cab6733107c551082db54a454876644c7
Emanuel <jose.eosousa@gmail.com>
2021-09-06 14:36:45 -0300

    add search funcionality to Dashboard, now going to code the search results

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,22 +1,78 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { Container, Form } from 'react-bootstrap';
+
+import SpotifyWebApi from 'spotify-web-api-node';
 
 import useAuth from './useAuth';
 
-import { Container, Form } from 'react-bootstrap';
+const spotifyApi = new SpotifyWebApi({
+    clientId: '6eb3f3a2db404366aa57361631ad8653',
+});
+
 
 function Dashboard({ code }) {
     const accessToken = useAuth(code);
 
     const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        if (!accessToken) return;
+        spotifyApi.setAccessToken(accessToken);
+    }, [accessToken]);
+
+    useEffect(() => {
+        if (!search) return setSearchResults([]);
+        if (!accessToken) return;
+
+        let cancel = false;
+        spotifyApi.searchTracks(search).then(res => {
+            if (cancel) return;
+
+            setSearchResults(res.body.tracks.items.map(track => {
+                const smallestAlbumImage = track.album.images.reduce(
+                    (smallest, image) => {
+                        if (image.height < smallest.height) return image;
+                        return smallest;
+                    }, track.album.images[0]);
+
+                return {
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }));
+        })
+
+        return () => cancel = true;
+    }, [search, accessToken]);
 
     return (
-        <Container>
+        <Container
+            className="d-flex flex-column py-2"
+            style={{ height: '100vh' }}
+        >
             <Form.Control
                 type="Search"
                 placeholder="Search Songs/Artists"
-                value={search} 
-                onChange={e => setSearch(e.target.value)} 
+                value={search}
+                onChange={e => setSearch(e.target.value)}
             />
+
+            <div
+                className="flex-grow-1 my-2"
+                style={{ overflowY: 'auto' }}
+            >
+                {searchResults.map(track => {
+                    <TrackSearchResult
+                        track={track}
+                        key={track.uri} 
+                    />
+                })}
+            </div>
+            <div>Bottom</div>
         </Container>
     );
 }
